370d0bf2d193a09a8a781592351e737a8bd6e9f1
Kajan1226 <vkkajan26@gmail.com>
2023-09-11 09:32:02 +0530

    Front Design Implementation

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import 'swiper/swiper-bundle.css';
-//  import 'swiper/swiper-bundle.js';
+import './custom.css';
+import './main.js';
 import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'boxicons/css/boxicons.min.css';
+import 'glightbox/dist/css/glightbox.min.css';
+import 'remixicon/fonts/remixicon.css';
+// import 'swiper/swiper-bundle.min.css';
+
 import 'aos/dist/aos.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'glightbox/dist/js/glightbox.min.js';
+import 'isotope-layout/dist/isotope.pkgd.min.js';
+// import 'swiper/swiper-bundle.min.js';
+import 'waypoints/lib/noframework.waypoints.js';
+
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
@@ -17,6 +29,7 @@ root.render(
     < App />
   </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
