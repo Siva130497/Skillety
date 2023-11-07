91fd75a5e7981004d621ba8c7ed95a9c9479725f
Kajan1226 <vkkajan26@gmail.com>
2023-11-08 01:09:07 +0530

    Signed-off-by: Kajan1226 <vkkajan26@gmail.com>

--- dashboard/src/pages/ManageJobs/ManageJobs.jsx
+++ dashboard/src/pages/ManageJobs/ManageJobs.jsx
@@ -11,11 +11,11 @@ import { useNavigate } from 'react-router-dom';
 
 const ManageJobs = () => {
     const [clientToken, setClientToken] = useState("");
-    const {getProtectedData} = useContext(AuthContext);
+    const { getProtectedData } = useContext(AuthContext);
     const [employeeId, setEmployeeId] = useState("");
     const [loginClientDetail, setLoginClientDetail] = useState([]);
     const [postedJobs, setPostedJobs] = useState([]);
-    const [appliedOfPostedJobs, setAppliedOfPostedJobs] =useState([]);
+    const [appliedOfPostedJobs, setAppliedOfPostedJobs] = useState([]);
     const [allStaff, setAllStaff] = useState([]);
 
     const navigate = useNavigate();
@@ -26,130 +26,130 @@ const ManageJobs = () => {
 
     }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         setClientToken(JSON.parse(localStorage.getItem('clientToken')))
-    },[clientToken])
+    }, [clientToken])
 
     useEffect(() => {
-        if(clientToken){
+        if (clientToken) {
             const fetchData = async () => {
                 try {
-                const user = await getProtectedData(clientToken);
-                console.log(user);
-                setEmployeeId(user.id);
+                    const user = await getProtectedData(clientToken);
+                    console.log(user);
+                    setEmployeeId(user.id);
                 } catch (error) {
-                console.log(error);
+                    console.log(error);
                 }
             };
-        
+
             fetchData();
         }
     }, [clientToken]);
 
-    const getLoginClientDetail = async() => {
-        try{
+    const getLoginClientDetail = async () => {
+        try {
             const res = await axios.get(`http://localhost:5002/client/${employeeId}`, {
-              headers: {
-                  Authorization: `Bearer ${clientToken}`,
-                  Accept: 'application/json'
-              }
+                headers: {
+                    Authorization: `Bearer ${clientToken}`,
+                    Accept: 'application/json'
+                }
             });
             const result = res.data;
             if (!result.error) {
-              console.log(result);
-              setLoginClientDetail(result);
+                console.log(result);
+                setLoginClientDetail(result);
             } else {
-              console.log(result);
+                console.log(result);
             }
-        }catch(err){
-          console.log(err);
+        } catch (err) {
+            console.log(err);
         }
-      }
+    }
 
-      useEffect(()=>{
-        if(employeeId){
-          getLoginClientDetail();
+    useEffect(() => {
+        if (employeeId) {
+            getLoginClientDetail();
         }
-      },[employeeId]);
+    }, [employeeId]);
 
-      const getOwnPostedjobs = async() => {
-        try{
+    const getOwnPostedjobs = async () => {
+        try {
             const res = await axios.get(`http://localhost:5002/my-posted-jobs/${loginClientDetail.companyId}`, {
-              headers: {
-                  Authorization: `Bearer ${clientToken}`,
-                  Accept: 'application/json'
-              }
+                headers: {
+                    Authorization: `Bearer ${clientToken}`,
+                    Accept: 'application/json'
+                }
             });
             const result = res.data;
             if (!result.error) {
-              console.log(result);
-              setPostedJobs(result);
+                console.log(result);
+                setPostedJobs(result);
             } else {
-              console.log(result);
+                console.log(result);
             }
-        }catch(err){
-          console.log(err);
+        } catch (err) {
+            console.log(err);
         }
-      }
+    }
 
-      const getAppliedOfPostedJobs = async() => {
-        try{
+    const getAppliedOfPostedJobs = async () => {
+        try {
             const res = await axios.get(`http://localhost:5002/applied-jobs-of-posted/${loginClientDetail.companyId}`, {
-              headers: {
-                  Authorization: `Bearer ${clientToken}`,
-                  Accept: 'application/json'
-              }
+                headers: {
+                    Authorization: `Bearer ${clientToken}`,
+                    Accept: 'application/json'
+                }
             });
             const result = res.data;
             if (!result.error) {
-              console.log(result);
-              setAppliedOfPostedJobs(result.reverse());
+                console.log(result);
+                setAppliedOfPostedJobs(result.reverse());
             } else {
-              console.log(result);
+                console.log(result);
             }
-        }catch(err){
-          console.log(err);
+        } catch (err) {
+            console.log(err);
         }
-      }
+    }
 
-      const allStaffFromCompany = async() => {
-        try{
+    const allStaffFromCompany = async () => {
+        try {
             const res = await axios.get(`http://localhost:5002/all-staff/${loginClientDetail.companyId}`, {
-              headers: {
-                  Authorization: `Bearer ${clientToken}`,
-                  Accept: 'application/json'
-              }
+                headers: {
+                    Authorization: `Bearer ${clientToken}`,
+                    Accept: 'application/json'
+                }
             });
             const result = res.data;
             if (!result.error) {
-              console.log(result);
-              setAllStaff(result);
+                console.log(result);
+                setAllStaff(result);
             } else {
-              console.log(result);
+                console.log(result);
             }
-        }catch(err){
-          console.log(err);
+        } catch (err) {
+            console.log(err);
         }
-      }
+    }
 
 
-    useEffect(()=>{
+    useEffect(() => {
         getOwnPostedjobs();
         getAppliedOfPostedJobs();
         allStaffFromCompany();
-      },[loginClientDetail]);
+    }, [loginClientDetail]);
 
     const handleDeleteJob = (id) => {
         axios.delete(`http://localhost:5002/delete-job/${id}`, {
-        headers: {
-            Authorization: `Bearer ${clientToken}`,
-            Accept: 'application/json'
-        }
+            headers: {
+                Authorization: `Bearer ${clientToken}`,
+                Accept: 'application/json'
+            }
         })
-        .then((res)=>console.log(res.data))
-        .catch((err)=>console.log(err))
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err))
     }
-    
+
     return (
         <div>
             <div class="main-wrapper main-wrapper-1">
@@ -180,41 +180,39 @@ const ManageJobs = () => {
 
                                                 {/* table data */}
                                                 {postedJobs.map((job) => {
-                                                const numApplicants = appliedOfPostedJobs.filter(appliedOfPostedJob => appliedOfPostedJob.jobId === job.id).length;
-                                                const staff = allStaff.find(obj => obj.id === (job.clientId || job.clientStaffId));
-                                                return (
-                                                    <tr className='dash-table-row client' key={job.id}>
-                                                        <td className='dash-table-data1'>{job.jobRole[0]}</td>
-                                                        <td className='dash-table-data1 text-center'>
-                                                            {`${new Date(job.createdAt).getDate().toString().padStart(2, '0')}/${(new Date(job.createdAt).getMonth() + 1).toString().padStart(2, '0')}/${new Date(job.createdAt).getFullYear() % 100}`}
-                                                        </td>
-                                                        <td className='dash-table-data1 text-center'>
-                                                            <button className='application-btn with-modal' onClick={()=>navigate(`/applied-candidate/${job.id}`)}>
-                                                                <span>{numApplicants}</span>&nbsp;&nbsp;&nbsp;
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
-                                                                    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z" fill='#0879bc' />
-                                                                </svg>
-                                                            </button>
-                                                        </td>
-                                                        <td className='dash-table-data1 text-center'>
-                                                            {staff ? staff.name : 'Unknown'}
-                                                        </td>
-                                                        <td className='text-center'>
-                                                            <button className='man-job-status-btn theme-info'>Ongoing</button>
-                                                        </td>
-                                                        <td className='text-center'>
-                                                            <button className='delete-btn' onClick={()=>handleDeleteJob(job.id)}>
-                                                                <i className="bi bi-trash"></i>
-                                                            </button>
-                                                        </td>
-                                                        <td className='text-center'>
-                                                            <button className='delete-btn' onClick={()=>navigate(`/edit-job/${job.id}`)}>
-                                                                {/* <i className="bi bi-trash"></i> */}edit
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                );
-                                            })}
+                                                    const numApplicants = appliedOfPostedJobs.filter(appliedOfPostedJob => appliedOfPostedJob.jobId === job.id).length;
+                                                    const staff = allStaff.find(obj => obj.id === (job.clientId || job.clientStaffId));
+                                                    return (
+                                                        <tr className='dash-table-row client' key={job.id}>
+                                                            <td className='dash-table-data1'>{job.jobRole[0]}</td>
+                                                            <td className='dash-table-data1 text-center'>
+                                                                {`${new Date(job.createdAt).getDate().toString().padStart(2, '0')}/${(new Date(job.createdAt).getMonth() + 1).toString().padStart(2, '0')}/${new Date(job.createdAt).getFullYear() % 100}`}
+                                                            </td>
+                                                            <td className='dash-table-data1 text-center'>
+                                                                <button className='application-btn with-modal' onClick={() => navigate(`/applied-candidate/${job.id}`)}>
+                                                                    <span>{numApplicants}</span>&nbsp;&nbsp;&nbsp;
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
+                                                                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z" fill='#0879bc' />
+                                                                    </svg>
+                                                                </button>
+                                                            </td>
+                                                            <td className='dash-table-data1 text-center'>
+                                                                {staff ? staff.name : 'Unknown'}
+                                                            </td>
+                                                            <td className='text-center'>
+                                                                <button className='man-job-status-btn theme-info'>Ongoing</button>
+                                                            </td>
+                                                            <td className='text-center'>
+                                                                <button className='delete-btn' onClick={() => handleDeleteJob(job.id)}>
+                                                                    <i className="bi bi-trash"></i>
+                                                                </button>
+                                                                <button className='edit-btn' onClick={() => navigate(`/edit-job/${job.id}`)}>
+                                                                    <i class="bi bi-pencil-fill"></i>
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                    );
+                                                })}
                                             </table>
                                         </div>
 
