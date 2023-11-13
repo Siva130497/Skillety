bdeb5cf37b1ce24bddcbe3e4005d6418b7e37958
Kajan1226 <vkkajan26@gmail.com>
2023-11-13 16:37:12 +0530

    Signed-off-by: Kajan1226 <vkkajan26@gmail.com>

--- frontend/src/components/NewNavCandidateHome.jsx
+++ frontend/src/components/NewNavCandidateHome.jsx
@@ -99,7 +99,7 @@ export const NewNavCandidateHome = ({homeActive, aboutUsActive, searchJobActive,
                         <li><a href="/talent-contact-us" className={contactActive ? "nav-link scrollto active" : "nav-link scrollto"}>Contact</a></li>
                         {userName ?
                             <li className="dropdown"><a href='#'><span>{extractLastName()}</span><i className="bi bi-chevron-down"></i></a>
-                                <ul>
+                                <ul className='loged-in'>
                                     <li><a href={`http://localhost:3000/${candidateToken}`} target='_blank'>Dash Board</a></li>
                                     <li onClick={() => {
                                         localStorage.removeItem("candidateToken");
